c1a1aac286a7a91ac190f11d381bb7940b52990c
enzo bertrand <enzo.berbertrand@gmail.com>
2026-04-14 20:02:42 +0200

    Reset du projet

--- pages/api/auth/me.js
+++
@@ -1,7 +0,0 @@
-import { getUserFromRequest } from '../../../lib/auth';
-
-export default function handler(req, res) {
-  const user = getUserFromRequest(req);
-  if (!user) return res.status(200).json({ user: null });
-  res.status(200).json({ user });
-}
--- pages/api/auth/questions.js
+++
@@ -1,6 +0,0 @@
-import { prisma } from '../../../lib/prisma';
-
-export default async function handler(req, res) {
-  const questions = await prisma.securityQuestion.findMany({ orderBy: { id: 'asc' } });
-  res.status(200).json({ questions });
-}
